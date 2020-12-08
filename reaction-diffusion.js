6227f3ed96356ff56fb1bbe999031d73aae6bc16
Sarcoma <sarcoma@live.co.uk>
2020-12-08 23:28:09 +0000

    Add tweaks

--- reaction-diffusion.js
+++ reaction-diffusion.js
@@ -1,8 +1,8 @@
-const gridWidth = 10;
-const gridHeight = 10;
+const gridWidth = 20;
+const gridHeight = 20;
 
 function initGridCell(x, y) {
-    if((x >= 4 && x <= 6) && (y >= 4 && y <= 6)) return {a: 1, b: 1}
+    if((x >= 8 && x <= 12) && (y >= 8 && y <= 12)) return {a: 1, b: 1}
     return {a: 1, b: 0};
 }
 
@@ -20,24 +20,25 @@ const laplacianMatrix = [
     [0.05, 0.2, 0.05],
 ]
 
-function diffusion(key, area) {
+function diffusion(key, area, laplacianMatrix) {
     let sum = 0;
+    let areaSum = 0;
     for(let x = 0; x < area.length; x++) {
         for(let y = 0; y < area[x].length; y++) {
-            sum += area[x][y][key] * laplacianMatrix[x][y]
+            sum += area[x][y][key] * laplacianMatrix[x][y];
+            areaSum += area[x][y][key]
         }
     }
-    const average = sum / 9;
-    console.log(average);
-    return average
+
+    return areaSum !== 0 ? sum / areaSum : 0;
 }
 
 function updateA(a, b, aDiffusion) {
-    return a + ((diffusionRateA * (aDiffusion * aDiffusion) * a) - a * (b * b) + feedRate * (1 - a)) * deltaTime;
+    return a + (diffusionRateA * (aDiffusion * aDiffusion) * a - a * b * b + feedRate * (1 - a)) * deltaTime;
 }
 
 function updateB(a, b, bDiffusion) {
-    return b + ((diffusionRateB * (bDiffusion * bDiffusion) * b) + a * (b * b) - (killRate + feedRate) * b) * deltaTime;
+    return b + (diffusionRateB * (bDiffusion * bDiffusion) * b + a * b * b - (killRate + feedRate) * b) * deltaTime;
 }
 
 function copyGrid(grid) {
@@ -51,9 +52,9 @@ function copyGrid(grid) {
 
 function makePattern(grid, width, height) {
     const pattern = [...Array(width)].map((_, x) => [...Array(height)].map((_, y) => ' '));
-    for (let x = 0; x < grid.length; x++) {
-        for(let y = 0; y < grid[x].length; y++) {
-            pattern[x][y] = grid[x][y].a < grid[x][y].b ? 'X' : '0'
+    for (let x = 1; x < grid.length - 1; x++) {
+        for(let y = 1; y < grid[x].length -1; y++) {
+            pattern[x][y] = grid[x][y].a < grid[x][y].b ? 'X' : ' '
         }
     }
     return pattern;
@@ -70,23 +71,18 @@ function drawPattern(pattern) {
     return str;
 }
 
-
-
 function run(grid) {
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 33; i++) {
         const newGrid = copyGrid(grid);
         for(let x = 1; x < grid.length - 1; x++) {
             for(let y = 1; y < grid[x].length - 1; y++) {
-                const aDiffusion = diffusion('a',[
+                const area = [
                     [grid[x-1][y-1], grid[x-1][y], grid[x-1][y+1]],
                     [grid[x][y-1], grid[x][y], grid[x][y+1]],
                     [grid[x+1][y-1], grid[x+1][y], grid[x+1][y+1]],
-                ]);
-                const bDiffusion = diffusion('b',[
-                    [grid[x-1][y-1], grid[x-1][y], grid[x-1][y+1]],
-                    [grid[x][y-1], grid[x][y], grid[x][y+1]],
-                    [grid[x+1][y-1], grid[x+1][y], grid[x+1][y+1]],
-                ]);
+                ];
+                const aDiffusion = diffusion('a', area, laplacianMatrix);
+                const bDiffusion = diffusion('b', area, laplacianMatrix);
                 grid[x][y].a = updateA(grid[x][y].a, grid[x][y].b, aDiffusion);
                 grid[x][y].b = updateB(grid[x][y].a, grid[x][y].b, bDiffusion);
             }
@@ -95,6 +91,5 @@ function run(grid) {
     }
     console.log(drawPattern(makePattern(grid, gridWidth, gridHeight)));
 }
-
 
 run(grid);
