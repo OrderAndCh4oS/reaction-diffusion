43c334991a91cc3dc1bde4470f09f9c4a7c0b610
Sarcoma <sarcoma@live.co.uk>
2020-12-09 22:05:44 +0000

    Tweaks

--- reaction-diffusion.js
+++ reaction-diffusion.js
@@ -12,7 +12,7 @@ const gridWidth = size;
 const gridHeight = size;
 
 function initGridCellBox(x, y) {
-    if((x >= 45 * 2.5 && x <= 55 * 2.5) && (y >= 45 * 2.5 && y <= 55 * 2.5)) return {a: 0, b: 1};
+    if((x >= 40 * 2.5 && x <= 50 * 2.5) && (y >= 40 * 2.5 && y <= 50 * 2.5)) return {a: 0, b: 1};
     return {a: 1, b: 0};
 }
 
@@ -25,8 +25,8 @@ let grid = [...Array(gridWidth)].map(
 
 const diffusionRateA = 1;
 const diffusionRateB = 0.5;
-const feedRate = 0.0367;
-const killRate = 0.0649;
+const feedRate = 0.0545;
+const killRate = 0.062;
 const deltaTime = 1;
 
 const laplacianMatrix = [
@@ -41,6 +41,7 @@ function round(value, precision) {
 }
 
 function diffusion(key, area, laplacianMatrix) {
+    // If laplacianSum does not equal zero divide sum by it.
     // const laplacianSum = laplacianMatrix.flat(1).reduce((a, b) => round(a + b, 2), 0);
     let sum = 0;
     for(let x = 0; x < area.length; x++) {
@@ -51,23 +52,6 @@ function diffusion(key, area, laplacianMatrix) {
 
     return sum;
 }
-
-// // Guess at the need to spread the values out around the current cell
-// function applyDiffusion(key, value, x, y, grid, laplacianMatrix) {
-//     const top = x > 0 ? x - 1 : grid.length - 1;
-//     const bottom = x < grid.length - 1 ? x + 1 : 0;
-//     const left = y > 0 ? y - 1 : grid[x].length - 1;
-//     const right = y < grid[x].length - 1 ? y + 1 : 0;
-//     grid[top][left][key] = value * laplacianMatrix[0][0];
-//     grid[x][left][key] = value * laplacianMatrix[1][0];
-//     grid[bottom][left][key] = value * laplacianMatrix[2][0];
-//     grid[top][y][key] = value * laplacianMatrix[0][1];
-//     grid[bottom][y][key] = value * laplacianMatrix[2][1];
-//     grid[top][right][key] = value * laplacianMatrix[0][2];
-//     grid[x][right][key] = value * laplacianMatrix[1][2];
-//     grid[bottom][right][key] = value * laplacianMatrix[2][2];
-//     // grid[x][y][key] = grid[x][y][key] * laplacianMatrix[1][1];
-// }
 
 function updateA(a, b, aDiffusion) {
     return a + ((diffusionRateA * aDiffusion) - (a * b * b) + (feedRate * (1 - a))) * deltaTime;
@@ -84,27 +68,6 @@ function copyGrid(grid) {
         newGrid[i] = grid[i].slice(0);
     }
     return newGrid;
-}
-
-function makePattern(grid, width, height) {
-    const pattern = [...Array(width)].map((_, x) => [...Array(height)].map((_, y) => ' '));
-    for(let x = 0; x < grid.length; x++) {
-        for(let y = 0; y < grid[x].length; y++) {
-            pattern[x][y] = grid[x][y].a <= grid[x][y].b ? 'X' : ' ';
-        }
-    }
-    return pattern;
-}
-
-function drawPattern(pattern) {
-    let str = '';
-    for(const row of pattern) {
-        for(const cell of row) {
-            str += cell;
-        }
-        str += '\n';
-    }
-    return str;
 }
 
 function update(grid) {
@@ -127,34 +90,30 @@ function update(grid) {
 
             const a = updateA(grid[x][y].a, grid[x][y].b, aDiffusion);
             const b = updateB(grid[x][y].a, grid[x][y].b, bDiffusion);
-            // if(isNaN(a)) throw new Error('A is NaN');
-            // if(isNaN(b)) throw new Error('B is NaN');
+            if(isNaN(a)) throw new Error('A is NaN');
+            if(isNaN(b)) throw new Error('B is NaN');
             newGrid[x][y].a = a;
             newGrid[x][y].b = b;
-            // applyDiffusion('a', grid[x][y].a, x, y, newGrid, laplacianMatrix);
-            // applyDiffusion('b', grid[x][y].b, x, y, newGrid, laplacianMatrix);
         }
     }
 
     return newGrid;
 }
 
-let i = 0;
-
 function draw() {
+    for(let i = 0; i < 10000; i++) {
+        console.log(`iteration: ${i}`);
+        update(grid);
+    }
     context.clearRect(0, 0, canvas.width, canvas.height);
     for(let x = 1; x < grid.length - 1; x++) {
         for(let y = 1; y < grid[x].length - 1; y++) {
-            // if(grid[x][y].a > 1 || grid[x][y].a < 0) throw new Error('A is out of range');
-            // if(grid[x][y].b > 1 || grid[x][y].b < 0) throw new Error('B is out of range');
-            context.fillStyle = grid[x][y].a > grid[x][y].b ? 'white' : 'black';
+            if(grid[x][y].a > 1 || grid[x][y].a < 0) throw new Error('A is out of range');
+            if(grid[x][y].b > 1 || grid[x][y].b < 0) throw new Error('B is out of range');
+            context.fillStyle = `rgb(0, ${grid[x][y].a * 255}, ${(1 - grid[x][y].b) * 255})`;
             context.fillRect(x, y, 1, 1);
         }
     }
-    update(grid);
-    i++;
-    console.log(i);
-    setTimeout(draw, 100);
 }
 
 draw();
