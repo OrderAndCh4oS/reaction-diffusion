54205988a69528a5ab16a4ecf5d8f0fe1039eb99
Sarcoma <sarcoma@live.co.uk>
2020-12-10 21:35:56 +0000

    Optimisations

--- reaction-diffusion.js
+++ reaction-diffusion.js
@@ -20,8 +20,9 @@ function initGridCellRand(x, y) {
     return {a: 1, b: Math.random() > 0.5 ? 1 : 0};
 }
 
-let grid = [...Array(gridWidth)].map(
-    (_, x) => [...Array(gridHeight)].map((_, y) => initGridCellBox(x, y)));
+let gridFrom = [...Array(gridWidth)].map((_, x) => [...Array(gridHeight)].map((_, y) => initGridCellBox(x, y)));
+
+let gridTo = [...Array(gridWidth)].map((_, x) => [...Array(gridHeight)].map((_, y) => ({a: 1, b: 0})));
 
 const diffusionRateA = 1;
 const diffusionRateB = 0.5;
@@ -35,30 +36,13 @@ const laplacianMatrix = [
     [0.05, 0.2, 0.05],
 ];
 
-function round(value, precision) {
-    const multiplier = Math.pow(10, precision || 0);
-    return Math.round(value * multiplier) / multiplier;
-}
-
-function diffusion(key, area, laplacianMatrix) {
-    // If laplacianSum does not equal zero divide sum by it.
-    // const laplacianSum = laplacianMatrix.flat(1).reduce((a, b) => round(a + b, 2), 0);
-    let sum = 0;
-    for(let x = 0; x < area.length; x++) {
-        for(let y = 0; y < area[x].length; y++) {
-            sum = sum + area[x][y][key] * laplacianMatrix[x][y];
-        }
-    }
-
-    return sum;
-}
-
 function updateA(a, b, aDiffusion) {
     return a + ((diffusionRateA * aDiffusion) - (a * b * b) + (feedRate * (1 - a))) * deltaTime;
 }
 
 function updateB(a, b, bDiffusion) {
-    return b + (((diffusionRateB * bDiffusion) + (a * b * b)) - ((killRate + feedRate) * b)) * deltaTime;
+    return b + (((diffusionRateB * bDiffusion) + (a * b * b)) - ((killRate + feedRate) * b)) *
+        deltaTime;
 }
 
 function copyGrid(grid) {
@@ -70,47 +54,57 @@ function copyGrid(grid) {
     return newGrid;
 }
 
-function update(grid) {
-    const newGrid = copyGrid(grid);
-    for(let x = 0; x < grid.length; x++) {
-        for(let y = 0; y < grid[x].length; y++) {
-            const top = x > 0 ? x - 1 : grid.length - 1;
-            const bottom = x < grid.length - 1 ? x + 1 : 0;
-            const left = y > 0 ? y - 1 : grid[x].length - 1;
-            const right = y < grid[x].length - 1 ? y + 1 : 0;
+function update() {
+    for(let x = 0; x < gridFrom.length; x++) {
+        for(let y = 0; y < gridFrom[x].length; y++) {
+            const top = x > 0 ? x - 1 : gridFrom.length - 1;
+            const bottom = x < gridFrom.length - 1 ? x + 1 : 0;
+            const left = y > 0 ? y - 1 : gridFrom[x].length - 1;
+            const right = y < gridFrom[x].length - 1 ? y + 1 : 0;
 
-            const area = [
-                [grid[top][left], grid[top][y], grid[top][right]],
-                [grid[x][left], grid[x][y], grid[x][right]],
-                [grid[bottom][left], grid[bottom][y], grid[bottom][right]],
-            ];
+            let aDiffusion = 0;
 
-            const aDiffusion = diffusion('a', area, laplacianMatrix);
-            const bDiffusion = diffusion('b', area, laplacianMatrix);
+            aDiffusion += gridFrom[top][left]['a'] * laplacianMatrix[0][0];
+            aDiffusion += gridFrom[top][y]['a'] * laplacianMatrix[0][1];
+            aDiffusion += gridFrom[top][right]['a'] * laplacianMatrix[0][2];
+            aDiffusion += gridFrom[x][left]['a'] * laplacianMatrix[1][0];
+            aDiffusion += gridFrom[x][y]['a'] * laplacianMatrix[1][1];
+            aDiffusion += gridFrom[x][right]['a'] * laplacianMatrix[1][2];
+            aDiffusion += gridFrom[bottom][left]['a'] * laplacianMatrix[2][0];
+            aDiffusion += gridFrom[bottom][y]['a'] * laplacianMatrix[2][1];
+            aDiffusion += gridFrom[bottom][right]['a'] * laplacianMatrix[2][2];
 
-            const a = updateA(grid[x][y].a, grid[x][y].b, aDiffusion);
-            const b = updateB(grid[x][y].a, grid[x][y].b, bDiffusion);
-            if(isNaN(a)) throw new Error('A is NaN');
-            if(isNaN(b)) throw new Error('B is NaN');
-            newGrid[x][y].a = a;
-            newGrid[x][y].b = b;
+            let bDiffusion = 0;
+
+            bDiffusion += gridFrom[top][left]['b'] * laplacianMatrix[0][0];
+            bDiffusion += gridFrom[top][y]['b'] * laplacianMatrix[0][1];
+            bDiffusion += gridFrom[top][right]['b'] * laplacianMatrix[0][2];
+            bDiffusion += gridFrom[x][left]['b'] * laplacianMatrix[1][0];
+            bDiffusion += gridFrom[x][y]['b'] * laplacianMatrix[1][1];
+            bDiffusion += gridFrom[x][right]['b'] * laplacianMatrix[1][2];
+            bDiffusion += gridFrom[bottom][left]['b'] * laplacianMatrix[2][0];
+            bDiffusion += gridFrom[bottom][y]['b'] * laplacianMatrix[2][1];
+            bDiffusion += gridFrom[bottom][right]['b'] * laplacianMatrix[2][2];
+
+            const a = updateA(gridFrom[x][y].a, gridFrom[x][y].b, aDiffusion);
+            const b = updateB(gridFrom[x][y].a, gridFrom[x][y].b, bDiffusion);
+            gridTo[x][y].a = a;
+            gridTo[x][y].b = b;
         }
     }
-
-    return newGrid;
+    [gridFrom, gridTo] = [gridTo, gridFrom];
 }
 
 function draw() {
-    for(let i = 0; i < 10000; i++) {
-        console.log(`iteration: ${i}`);
-        update(grid);
+    console.time('update');
+    for(let i = 0; i < 12500; i++) {
+        update();
     }
+    console.timeEnd('update');
     context.clearRect(0, 0, canvas.width, canvas.height);
-    for(let x = 1; x < grid.length - 1; x++) {
-        for(let y = 1; y < grid[x].length - 1; y++) {
-            if(grid[x][y].a > 1 || grid[x][y].a < 0) throw new Error('A is out of range');
-            if(grid[x][y].b > 1 || grid[x][y].b < 0) throw new Error('B is out of range');
-            context.fillStyle = `rgb(0, ${grid[x][y].a * 255}, ${(1 - grid[x][y].b) * 255})`;
+    for(let x = 1; x < gridFrom.length - 1; x++) {
+        for(let y = 1; y < gridFrom[x].length - 1; y++) {
+            context.fillStyle = `rgb(0, ${gridFrom[x][y].a * 255}, ${(1 - gridFrom[x][y].b) * 255})`;
             context.fillRect(x, y, 1, 1);
         }
     }
